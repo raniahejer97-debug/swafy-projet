c49a72b603c6bf7cc81727e898194f66a36176da
labedihejer-pixel <labedihejer@gmail.com>
2026-05-19 14:57:02 +0100

    fix backend mails + json

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://swafy-backend.onrender.com/api",
+  baseURL: "https://swafy-projet.onrender.com/api",
 });
 /* =========================
     RESPONSE INTERCEPTOR
